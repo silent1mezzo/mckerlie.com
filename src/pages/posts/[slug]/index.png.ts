463b24fa698cff7fe3ba296249a1486c1cdd7438
Adam McKerlie <adammckerlie@gmail.com>
2023-09-28 14:58:06 -0400

    Update slugs

--- src/pages/posts/[slug]/index.png.ts
+++ src/pages/posts/[slug]/index.png.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from "astro";
 import { getCollection, type CollectionEntry } from "astro:content";
 import { generateOgImageForPost } from "@utils/generateOgImages";
-import { slugifyStr } from "@utils/slugify";
+import slugify from "@utils/slugify";
 
 export async function getStaticPaths() {
   const posts = await getCollection("posts").then(p =>
@@ -9,7 +9,7 @@ export async function getStaticPaths() {
   );
 
   return posts.map(post => ({
-    params: { slug: slugifyStr(post.data) },
+    params: { slug: slugify(post.data) },
     props: post,
   }));
 }
