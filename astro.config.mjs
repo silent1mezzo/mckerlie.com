9b60a57756389f9fb468821dc97ce5821d13c203
Adam McKerlie <adammckerlie@gmail.com>
2023-10-05 22:25:48 -0400

    Fix base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  site: "https://mckerlie.com", // replace this with your deployed domain
   integrations: [
     tailwind({
       applyBaseStyles: false,
