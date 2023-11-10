1460d33dbe1784b45677cd0a113c8f77789b1787
Adam McKerlie <adammckerlie@gmail.com>
2023-11-10 10:23:18 -0500

    Update Sentry to use official astro sdk

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,43 +8,46 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { autolinkConfig } from './plugins/rehype-autolink-config';
 import sitemap from "@astrojs/sitemap";
 import mdx from '@astrojs/mdx';
+import sentry from "@sentry/astro";
+
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mckerlie.com", // replace this with your deployed domain
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
+  site: "https://mckerlie.com",
+  // replace this with your deployed domain
+  integrations: [tailwind({
+      applyBaseStyles: false
     }),
     react(),
     sitemap(),
     mdx(),
+    sentry({
+      dsn: "https://3ec5190943c3445d314ff2144751d8c1@o436943.ingest.sentry.io/4505986690973696",
+      replaysSessionSampleRate: 1.0,
+      environment: process.env.SENTRY_ENVIRONMENT,
+      clientInitPath: "sentry.client.config.js",
+      sourceMapsUploadOptions: {
+        project: "mckerliecom",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      }
+    })
   ],
   markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: "Table of contents",
-        },
-      ],
-    ],
-    rehypePlugins: [
-			rehypeSlug,
-      [rehypeAutolinkHeadings, autolinkConfig],
-    ],
+    remarkPlugins: [remarkToc, [remarkCollapse, {
+      test: "Table of contents"
+    }]],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, autolinkConfig]],
     shikiConfig: {
       theme: "one-dark-pro",
-      wrap: true,
+      wrap: true
     },
-    extendDefaultPlugins: true,
+    extendDefaultPlugins: true
   },
   vite: {
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
+      exclude: ["@resvg/resvg-js"]
+    }
   },
   scopedStyleStrategy: "where",
-  trailingSlash: "never",
+  trailingSlash: "never"
 });
