3951616726b39fbabcad5c7449943a3603fff88e
Adam McKerlie <adam.mckerlie@replicant.ai>
2024-08-14 16:07:55 -0400

    Remove Sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { autolinkConfig } from './plugins/rehype-autolink-config';
 import sitemap from "@astrojs/sitemap";
 import mdx from '@astrojs/mdx';
-import sentry from "@sentry/astro";
 
 
 // https://astro.build/config
@@ -20,17 +19,7 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
-    mdx(),
-    sentry({
-      dsn: "https://3ec5190943c3445d314ff2144751d8c1@o436943.ingest.sentry.io/4505986690973696",
-      replaysSessionSampleRate: 1.0,
-      environment: process.env.SENTRY_ENVIRONMENT,
-      clientInitPath: "sentry.client.config.js",
-      sourceMapsUploadOptions: {
-        project: "mckerliecom",
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-      }
-    })
+    mdx()
   ],
   markdown: {
     remarkPlugins: [remarkToc, [remarkCollapse, {
